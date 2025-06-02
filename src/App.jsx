5a049d5c92c744e31ffd521839d6db52ba429e34
Nastya <yanyshynnastya3@gmail.com>
2025-06-02 14:54:05 +0300

    feat(fix): fix gh-pages bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SignUp from './pages/SignUp';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+ 
 export default function App() {
   return (
-    <Router>
+    <Router>  {/* Remove basename completely */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
--- src/shared/ui/Header.jsx
+++ src/shared/ui/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Navbar, NavbarBrand, NavbarToggle, NavbarCollapse, NavbarLink,  Button } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { Navbar, NavbarBrand, NavbarToggle, NavbarCollapse, NavbarLink, Button } from 'flowbite-react';
 import { HiOutlineHome, HiOutlineViewGrid } from 'react-icons/hi';
 import { HiOutlineArchiveBox } from 'react-icons/hi2';
 
 function Header() {
   return (
     <Navbar fluid rounded className="bg-white shadow-sm">
-      <NavbarBrand href="/">
+      <NavbarBrand as={Link} to="/">
         <HiOutlineArchiveBox className="mr-3 h-6 w-6 text-blue-600" />
         <span className="self-center whitespace-nowrap text-xl font-semibold text-white">
           Freezer Manager
@@ -14,21 +15,25 @@ function Header() {
       </NavbarBrand>
       
       <div className="flex md:order-2 gap-2">
-        <Button color="light" size="sm" href="/login">
-          Login
-        </Button>
-        <Button color="blue" size="sm" href="/signup">
-          Sign Up
-        </Button>
+        <Link to="/login">
+          <Button color="light" size="sm">
+            Login
+          </Button>
+        </Link>
+        <Link to="/signup">
+          <Button color="blue" size="sm">
+            Sign Up
+          </Button>
+        </Link>
         <NavbarToggle />
       </div>
       
       <NavbarCollapse>
-        <NavbarLink href="/" className="flex items-center gap-2">
+        <NavbarLink as={Link} to="/" className="flex items-center gap-2">
           <HiOutlineHome className="h-4 w-4" />
           Home
         </NavbarLink>
-        <NavbarLink href="/dashboard" className="flex items-center gap-2">
+        <NavbarLink as={Link} to="/dashboard" className="flex items-center gap-2">
           <HiOutlineViewGrid className="h-4 w-4" />
           Dashboard
         </NavbarLink>
